7f9f480f0eb21b2869a8efa98292001fef90afed
柒七 <12486942+chen-duxiong@user.noreply.gitee.commkdir>
2023-11-12 22:39:55 +0800

    springboot准备完成

---
+++ springbootziji/src/main/java/com/cdx/springbootziji/SpringbootzijiApplication.java
@@ -0,0 +1,13 @@
+package com.cdx.springbootziji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootzijiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootzijiApplication.class, args);
+	}
+
+}
---
+++ springbootziji/src/test/java/com/cdx/springbootziji/SpringbootzijiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdx.springbootziji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootzijiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
